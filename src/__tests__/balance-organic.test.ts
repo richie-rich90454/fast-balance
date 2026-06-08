fa50de38c26c05e1d5d40f79e2d9f4923dfd9eca
richie-rich90454 <rjiang880@gmail.com>
2026-06-08 15:23:16 +0800

    test alkene and alkyne combustion

--- src/__tests__/balance-organic.test.ts
+++ src/__tests__/balance-organic.test.ts
@@ -33,3 +33,26 @@ describe("alkane combustion", () => {
     expect(r.products.map(x => x.coefficient)).toEqual([12, 14]);
   });
 });
+
+describe("alkene and alkyne combustion", () => {
+  it("balances C2H4 + 3O2 -> 2CO2 + 2H2O", () => {
+    const r = balance("C2H4 + O2 -> CO2 + H2O");
+    expect(r.reactants.map(x => x.coefficient)).toEqual([1, 3]);
+    expect(r.products.map(x => x.coefficient)).toEqual([2, 2]);
+  });
+  it("balances 2C2H2 + 5O2 -> 4CO2 + 2H2O", () => {
+    const r = balance("C2H2 + O2 -> CO2 + H2O");
+    expect(r.reactants.map(x => x.coefficient)).toEqual([2, 5]);
+    expect(r.products.map(x => x.coefficient)).toEqual([4, 2]);
+  });
+  it("balances C3H6 + 9/2 O2 -> 3CO2 + 3H2O (2C3H6 + 9O2 -> 6CO2 + 6H2O)", () => {
+    const r = balance("C3H6 + O2 -> CO2 + H2O");
+    expect(r.reactants.map(x => x.coefficient)).toEqual([2, 9]);
+    expect(r.products.map(x => x.coefficient)).toEqual([6, 6]);
+  });
+  it("balances C4H8 + 6O2 -> 4CO2 + 4H2O", () => {
+    const r = balance("C4H8 + O2 -> CO2 + H2O");
+    expect(r.reactants.map(x => x.coefficient)).toEqual([1, 6]);
+    expect(r.products.map(x => x.coefficient)).toEqual([4, 4]);
+  });
+});
