f5912866aa07b091ab022d3bc5ad505a4b0b00cf
richie-rich90454 <rjiang880@gmail.com>
2026-07-03 11:53:46 +0800

    style: implement chemistry-themed custom CSS with unique visual identity

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,6 +1,7 @@
 import DefaultTheme from 'vitepress/theme';
 import type { Theme } from 'vitepress';
 import BalanceDemo from '../components/BalanceDemo.vue';
+import './custom.css';
 export default {
     extends: DefaultTheme,
     enhanceApp({ app }) {
