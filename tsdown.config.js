a78c29ec279c9996802f0afafa71ff7d1ddce6e8
richie-rich90454 <rjiang880@gmail.com>
2026-07-16 22:45:14 +0800

    replace pure rolldown with tsdown

---
+++ tsdown.config.js
@@ -0,0 +1,13 @@
+import {defineConfig} from "tsdown"
+
+export default defineConfig({
+    entry: ["src/index.ts"],
+    format: ["esm", "cjs"],
+    dts: true,
+    minify: true,
+    target: "es2015",
+    sourcemap: false,
+    deps:{
+        neverBundle: ["chemparse"]
+    }
+})
