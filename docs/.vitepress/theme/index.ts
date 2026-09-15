adf3e1f6c924f252cf81ecac76065436b073bdb0
richie-rich90454 <rjiang880@gmail.com>
2026-09-15 19:58:23 +0800

    style: apply oxfmt to docs/.vitepress/theme/index.ts

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,10 +1,10 @@
-import DefaultTheme from 'vitepress/theme';
-import type { Theme } from 'vitepress';
-import BalanceDemo from '../components/BalanceDemo.vue';
-import './custom.css';
+import DefaultTheme from "vitepress/theme";
+import type { Theme } from "vitepress";
+import BalanceDemo from "../components/BalanceDemo.vue";
+import "./custom.css";
 export default {
     extends: DefaultTheme,
     enhanceApp({ app }) {
-        app.component('BalanceDemo', BalanceDemo);
-    }
+        app.component("BalanceDemo", BalanceDemo);
+    },
 } satisfies Theme;
