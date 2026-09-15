ee7b1e09117d1aaac2acf3a6066cbfd71840b015
richie-rich90454 <rjiang880@gmail.com>
2026-09-15 18:42:42 +0800

    build: target es2020 and remove neverBundle chemparse

--- tsdown.config.js
+++ tsdown.config.js
@@ -1,16 +1,13 @@
-import {defineConfig} from "tsdown"
+import { defineConfig } from "tsdown"
 
 export default defineConfig({
     entry: ["src/index.ts"],
     format: ["esm", "cjs"],
     dts: true,
     minify: true,
-    target: "es2015",
+    target: "es2020",
     sourcemap: false,
     inputOptions: {
         treeshake: true,
     },
-    deps:{
-        neverBundle: ["chemparse"]
-    }
 })
